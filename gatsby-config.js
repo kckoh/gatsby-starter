934ee22ebaddebc2f1d7e797f449b3a61cacf2e2
kckoh <kyungcheolkoh@gmail.com>
2021-11-28 20:23:39 -0800

    finished gatsby tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    title: "My First site with Gatsby",
   },
   plugins: [ "gatsby-plugin-image",
-  "gatsby-plugin-sharp",],
+  "gatsby-plugin-sharp",
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: `blog`,
+      path: `${__dirname}/blog`,
+    }
+  },
+],
 };
